01b879207e80c3ec8859054462ca3ee2f0e9588a
ArianHoffmann <arianhoffmann16@gmail.com>
2022-03-09 17:00:04 -0300

    Corrección Mayúsculas

--- src/general/Cart.js
+++ src/general/Cart.js
@@ -11,4 +11,4 @@ const Cart = () => {
   )
 }
 
-export default Cart
+export default Cart;
--- src/general/navbar.js
+++ src/general/navbar.js
@@ -3,7 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import {  Links, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import '../style/Navbar.css'
 
 
