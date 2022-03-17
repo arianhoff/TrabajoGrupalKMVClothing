21e2bf37db8138113fd4d6e5a4ef155882509cbe
Leonardo Trucco <leotrucco@yahoo.com.ar>
2022-03-17 11:44:18 -0300

    se añade ruteo sección redes sociales

--- src/general/routeKMV.js
+++ src/general/routeKMV.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./homeKMV"
 import Cart from "./cartKMV"
-import Navbar from "./navbarKMV";
+import Navbar from "./navbarKMV"
+import Redes from "./socialMediaKMV"
 
 const RoutesKMV = () => {
     return(   
@@ -10,9 +11,11 @@ const RoutesKMV = () => {
             <Routes>
                 <Route path="/" element={ <Home/>}/>
                 <Route path="/cart" element={ <Cart/>}/>
+                <Route path="/social" element={ <Redes/>}/>  
             </Routes>
         </Router>
     )
 }
 
 export default RoutesKMV;
+
