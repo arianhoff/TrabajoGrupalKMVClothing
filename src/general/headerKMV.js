47228818cc33c741d46fac7b74e4faf61c7ee49c
santilagos <sntlgs@gmail.com>
2022-03-21 10:28:34 -0300

    estructura

--- src/general/headerKMV.js
+++ src/general/headerKMV.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import logo from '../LogoKmv.jpeg';
 import '../style/headerKMV.css'
 
 const Header = () => {
   return (
-    <div>
-        <header className='Header'>
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>KMV Clothing</h1>
-        </header>
+  <div className='principal'>   
+    <div className='headerPortada' alt='da'>
+    <img ></img>
+    <h1 className='descripcionPortada'>buenas</h1>
+    
     </div>
+
+    
+    
+  </div>
   )
 }
 
