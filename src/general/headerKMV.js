8223f12e9ce4e09f4b9424a6e91ff2243b81c950
santilagos <sntlgs@gmail.com>
2022-03-21 16:37:59 -0300

    Portada1

--- src/general/headerKMV.js
+++ src/general/headerKMV.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import '../style/headerKMV.css'
-
+import Logo from "../assets/Logo.png"
+import { Box } from '@mui/system'
 const Header = () => {
   return (
   <div className='principal'>   
+
+
     <div className='headerPortada' alt='da'>
-    <img ></img>
-    <h1 className='descripcionPortada'></h1>
     
-    </div>
+      <Box xs={6} md={4} lg={2}>
+      <img src= {Logo} alt='imagensecundaria'></img>
+      <div className='descripcionPortada'>
+        <h1> Krueger, Myers and Voorhees Clothing Co. </h1>
+        <p> KMV es una casa de modas Argentina, fundada en Buenos Aires, en 2003. </p> 
+        <p> La marca se especializa en diseñar y confeccionar artículos de lujo.</p>
+      </div>
+    
+      </Box>
+      </div>
 
     
     
--- src/general/routeKMV.js
+++ src/general/routeKMV.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./homeKMV"
 import Cart from "./cartKMV"
 import Navbar from "./navbarKMV";
+import Header from "./headerKMV";
 
 const RoutesKMV = () => {
     return(   
         <Router>
             <Navbar/>
+            <Header />
             <Routes>
                 <Route path="/" element={ <Home/>}/>
                 <Route path="/cart" element={ <Cart/>}/>
