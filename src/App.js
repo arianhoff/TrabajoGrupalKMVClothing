03e02f1b3f00e30f61557365eae7754926949996
Juan Jose Lopez <jjlopezal@gmail.com>
2022-03-21 23:53:27 -0300

    Se arregla tamaño del background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import RoutesKMV from "../src/general/routeKMV";
-import Header from './general/headerKMV';
+
 
 function App() {
   return (
--- src/general/headerKMV.js
+++ src/general/headerKMV.js
@@ -2,26 +2,20 @@ import React from 'react'
 import '../style/headerKMV.css'
 import Logo from "../assets/Logo.png"
 import { Box } from '@mui/system'
+
 const Header = () => {
   return (
   <div className='principal'>   
-
-
-    <div className='headerPortada' alt='da'>
-        <Box xs={6} md={4} lg={2}>
-          <img src= {Logo} alt='imagensecundaria'></img>
-          <div className='descripcionPortada'>
-            <h1> Krueger, Myers and Voorhees Clothing Co. </h1>
-            <p> KMV es una casa de modas Argentina, fundada en Buenos Aires, en 2003. </p> 
-            <p> La marca se especializa en diseñar y confeccionar artículos de lujo.</p>
-          </div>
-        </Box>
-    </div>
-
-    
-    
+    <Box className='logo' >
+      <img src= {Logo} alt='LogoKmv'></img>
+    </Box>
+    <Box className='descripcionPortada'>
+      <p> Krueger, Myers and Voorhees Clothing Co.  <br/>
+          KMV es una casa de modas Argentina, fundada en Buenos Aires, en 2003.   <br/>
+          La marca se especializa en diseñar y confeccionar artículos de lujo. </p>
+    </Box>
   </div>
-  )
+  );
 }
 
 export default Header;
