578c6684be2d89c5d3489dfd54d2450790cb8745
ArianHoffmann <arianhoffmann16@gmail.com>
2022-03-28 20:34:03 -0300

    Avance Navbar / Falta logo, y ver títulos.

--- src/general/enviosKMV.js
+++ src/general/enviosKMV.js
@@ -1,11 +1,34 @@
-import React from "react";
-import "../style/enviosKMV.css";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
 
-const giftKMV = () => {
+export default function TransitionAlerts() {
+  const [open, setOpen] = React.useState(true);
+
   return (
-    <div className="envios">
-      <p>Hasta 12 cuotas y envio gratis con tu compra superior a $9000!</p>
-    </div>
+    <Box sx={{  width: '100%' }}>
+      <Collapse in={open}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          Hasta 12 cuotas y envio gratis con tu compra superior a $9000!
+        </Alert>
+      </Collapse>
+    </Box>
   );
-};
-export default giftKMV;
+}
--- src/general/navbarKMV.js
+++ src/general/navbarKMV.js
@@ -1,33 +1,117 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { NavLink } from "react-router-dom"
-import '../style/navbarKMV.css'
-
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import "../style/navbarKMV.css";
+import Envios from '../general/enviosKMV.js'
 
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const openMenu = Boolean(anchorEl);
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    console.log(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <Box sx={{ flexGrow: 1 }} className ="NavBar">
-    <AppBar>
+    <AppBar elevation={1} sx={{ backgroundColor: "#0F0F0D" }}>
+      <Envios />
       <Toolbar>
-        <Typography variant="h6" component="div"   style={{ width: "100%" }} 
-        sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          p: 1,
-          m: 1,
-          borderRadius: 1,
-        }}
-      >
-             <NavLink to="/" activeclassname="active"> Inicio</NavLink>
-             <NavLink to="/Cart" activeclassname="active"> Carrito</NavLink>
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+          component="div"
+        >
+          {/*Logo*/}
+          <Box></Box>
+          {/*Links*/}
+          <Box sx={{ display: "flex" }}>
+            <Typography
+              sx={{ marginRight: "20px", cursor: "pointer", color: "white" }}
+            >
+              Inicio
+            </Typography>
+            <Typography
+              sx={{ marginRight: "20px", cursor: "pointer", color: "white" }}
+              aria-controls="basic-menu"
+              aria-haspopup="true"
+              aria-expanded={openMenu ? "true" : undefined}
+              onClick={handleClick}
+            >
+              Categorías
+            </Typography>
+            {/*Dropdown Items */}
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={openMenu}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Camperas</MenuItem>
+              <MenuItem onClick={handleClose}>Pantalones</MenuItem>
+              <MenuItem onClick={handleClose}>Remeras</MenuItem>
+              <MenuItem onClick={handleClose}>Sweaters</MenuItem>
+              <MenuItem onClick={handleClose}>Ropa Interior</MenuItem>
+              <MenuItem onClick={handleClose}>Accesorios</MenuItem>
+            </Menu>
+            <Typography
+              sx={{ marginRight: "20px", cursor: "pointer", color: "white" }}
+            >
+              Ofertas
+            </Typography>
+            <Typography
+              sx={{ marginRight: "20px", cursor: "pointer", color: "white" }}
+            >
+              Nosotros
+            </Typography>
+          </Box>
+          {/*Botón*/}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <IconButton color="inherit">
+              <Badge>
+                <PersonRoundedIcon />
+              </Badge>
+            </IconButton>
+            <IconButton color="inherit">
+              <Badge>
+                <SearchRoundedIcon />
+              </Badge>
+            </IconButton>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="primary">
+                <ShoppingCartRoundedIcon color="inherit" />
+              </Badge>
+            </IconButton>
+          </Box>
+        </Box>
       </Toolbar>
     </AppBar>
-  </Box>
+  );
+};
 
-  )
-}
 export default Navbar;
