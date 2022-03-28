e01d3a9d6a397d5c1f675e2b33ed5f21fb730f0b
Macssimo <72900412+Macssimo@users.noreply.github.com>
2022-03-28 14:01:03 -0300

    Tarea terminada

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import RoutesKMV from "../src/general/routeKMV";
-import Header from './general/headerKMV';
-import Article from "./general/article";
 
 function App() {
-  return (
-    <div>
-			<Header />
+	return (
+			<>
 			<RoutesKMV />
-			<Article />
-    </div>
+			</>
   );
 }
 export default App;
--- src/general/routeKMV.js
+++ src/general/routeKMV.js
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./homeKMV"
-import Cart from "./cartKMV"
+import Home from "./homeKMV";
+import Cart from "./cartKMV";
 import Navbar from "./navbarKMV";
+import Article from "./article";
 
 const RoutesKMV = () => {
-    return(   
-        <Router>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={ <Home/>}/>
-                <Route path="/cart" element={ <Cart/>}/>
-            </Routes>
-        </Router>
-    )
+	return (
+		<Router>
+			<Navbar />
+			<Article />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/cart" element={<Cart />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default RoutesKMV;
