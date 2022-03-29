8c5d11f39c8eba3bb7aa16be8399cba0f99a750a
ArianHoffmann <arianhoffmann16@gmail.com>
2022-03-28 23:45:23 -0300

    Guarda cambios de drawerComponent

--- src/general/drawerComponent.js
+++ src/general/drawerComponent.js
@@ -1,45 +1,35 @@
-import React from 'react'
-import { Drawer } from '@mui/material'
-import { ListItem, List, ListItemIcon } from '@mui/material'
-import { ListItemText } from '@mui/material'
+import React from "react";
+import { Drawer } from "@mui/material";
+import { ListItem, List, ListItemIcon } from "@mui/material";
+import { ListItemText } from "@mui/material";
 
-
-
-const DrawerComponent = ({openDrawer, setOpenDrawer}) => {
+const DrawerComponent = ({ openDrawer, setOpenDrawer }) => {
   return (
     <Drawer anchor="right" open={openDrawer}>
-        <List>
-            <ListItem divider button onClick={()=>setOpenDrawer(false)}>
-                <ListItemIcon>
-                    <ListItemText>
-                        Categorías
-                    </ListItemText>
-                </ListItemIcon>
-            </ListItem>
-            <ListItem divider button onClick={()=>setOpenDrawer(false)}>
-                <ListItemIcon>
-                    <ListItemText>
-                        Ofertas
-                    </ListItemText>
-                </ListItemIcon>
-            </ListItem>
-            <ListItem divider button onClick={()=>setOpenDrawer(false)}>
-                <ListItemIcon>
-                    <ListItemText>
-                        Nosotros
-                    </ListItemText>
-                </ListItemIcon>
-            </ListItem>
-            <ListItem divider button onClick={()=>setOpenDrawer(false)}>
-                <ListItemIcon>
-                    <ListItemText>
-                        FAQ´s
-                    </ListItemText>
-                </ListItemIcon>
-            </ListItem>
-        </List>
+      <List>
+        <ListItem divider button onClick={() => setOpenDrawer(false)}>
+          <ListItemIcon>
+            <ListItemText>Categorías</ListItemText>
+          </ListItemIcon>
+        </ListItem>
+        <ListItem divider button onClick={() => setOpenDrawer(false)}>
+          <ListItemIcon>
+            <ListItemText>Ofertas</ListItemText>
+          </ListItemIcon>
+        </ListItem>
+        <ListItem divider button onClick={() => setOpenDrawer(false)}>
+          <ListItemIcon>
+            <ListItemText>Nosotros</ListItemText>
+          </ListItemIcon>
+        </ListItem>
+        <ListItem divider button onClick={() => setOpenDrawer(false)}>
+          <ListItemIcon>
+            <ListItemText>FAQ´s</ListItemText>
+          </ListItemIcon>
+        </ListItem>
+      </List>
     </Drawer>
-  )
-}
+  );
+};
 
 export default DrawerComponent;
