ee8e5500cf50ec7b747a95167a00a97b59c126a2
santilagos <sntlgs@gmail.com>
2022-03-29 11:32:29 -0300

    primer adelanto basico

--- src/general/Proveedor.js
+++ src/general/Proveedor.js
@@ -2,18 +2,20 @@
 
 import { Box, Button } from '@mui/material'
 import React from 'react'
+import '../style/Proveedor.css'
+import sarkany from '../assets/sarkany.png'
 
 
 const Proveedor = () => {
   return (
-    <Box>
-        hola
-        {/* <Button color="default">
-            ¡Descubre la coleccion de otoño!
-        </Button> */}
+    <Box className="holas" textAlign={'center'} height={'auto'} >
+      <img src={sarkany} className='img-proveedor'></img>
+      <br/>
+      <Button href='https://www.rickysarkany.com/' style={{margin:'100px 100px'}} color='primary' variant='contained'>
+          ¡Descubre la coleccion de otoño!
+      </Button>
     </Box>
-    
-  )
+  );
 }
 
 export default Proveedor
