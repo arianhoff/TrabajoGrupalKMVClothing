a0b1c0a71ff1b9b36db43a0e9ac4e50a280c8931
ArianHoffmann <arianhoffmann16@gmail.com>
2022-04-18 17:57:14 -0300

    Estilo provedor

--- src/general/Proveedor.js
+++ src/general/Proveedor.js
@@ -3,16 +3,14 @@
 import { Box, Button } from '@mui/material'
 import React from 'react'
 import '../style/Proveedor.css'
-import sarkany from '../assets/sarkany.png'
+import Sarkany from '../assets/Sarkany.jpg'
 
 
 const Proveedor = () => {
   return (
-    <Box className="holas" textAlign={'center'} height={'auto'} >
-      <img src={sarkany} alt="Logo-Sarkany" className='img-proveedor'></img>
-      <br/>
-      <Button href='https://www.rickysarkany.com/' style={{margin:'0px 100px 90px 100px'}} color='primary' variant='contained'>
-          ¡New colecction  winter 22!
+    <Box className="box-principal" textAlign={'center'} height={'381px'} >
+      <Button href='https://www.rickysarkany.com/' style={{marginTop:250}} color='primary' variant='contained'>
+          ¡Nueva colección invierno 2022!
       </Button>
     </Box>
   );
